2654f51f0ef738833c9658c347df11ee5badfacd
albertiriveiro-droid <albertiriveiro@gmail.com>
2026-01-08 18:03:30 +0100

    Agregamos mejoras en las dynamic odds

--- BetTime.API/Program.cs
+++ BetTime.API/Program.cs
@@ -62,7 +62,7 @@ builder.Services.AddScoped<ITransactionRepository, TransactionEFRepository>();
 builder.Services.AddScoped<IBetRepository, BetEFRepository>();
 
 
-var connectionString = builder.Configuration.GetConnectionString("ServerDB_azure");
+var connectionString = builder.Configuration.GetConnectionString("ServerDB_dockernet");
 
 builder.Services.AddDbContext<BetTimeContext>(options =>
     options.UseSqlServer(connectionString));
